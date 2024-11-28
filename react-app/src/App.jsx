5efac31bc5ff0423d891753aa6dad04017c6d8d0
sanghun <vitash1215@gmail.com>
2024-11-29 02:43:33 +0900

    feat : Page 라우팅

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,11 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AnalysisPage from './pages/AnalysisPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AnalysisPage from "./pages/AnalysisPage";
+import ScrapPage from "./pages/ScrapPage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/AnalysisPage" element={<AnalysisPage />} /> {/* 분석 차트 페이지 라우팅 */}
+        <Route path="/AnalysisPage" element={<AnalysisPage />} />{" "}
+        {/* 분석 차트 페이지 라우팅 */}
+        <Route path="/scrap" element={<ScrapPage />} />
       </Routes>
     </Router>
   );
